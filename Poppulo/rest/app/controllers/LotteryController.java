d3cf0583b39a4d325ecdaa2c63bff8dae2d8ad29
john-lay <john_lay@hotmail.co.uk>
2019-06-26 21:48:50 +0100

    Amend a ticket
    Add example curl requests for operations.

--- Poppulo/rest/app/controllers/LotteryController.java
+++ Poppulo/rest/app/controllers/LotteryController.java
@@ -25,6 +25,8 @@ public class LotteryController extends Controller {
     /**
      * Lists all lottery tickets
      *
+     * Example: curl -X GET http://localhost:9000/ticket
+     *
      * @return A http 200 response with a body containing all tickets as json
      */
     public Result allTickets() {
@@ -34,22 +36,23 @@ public class LotteryController extends Controller {
     /**
      * Generate a new ticket and add it to the collection of tickets
      *
+     * Example (with 3 lines): curl -X POST http://localhost:9000/ticket/3
+     *
      * @param numberOfLines the number of lines to add to the ticket
      * @return A http 200 response with a body containing the new ticket
      */
     public Result createTicket(int numberOfLines) {
         Ticket t = new Ticket();
-        for (int i = 0; i < numberOfLines; i++) {
-            int[] line = {newTicketNumber(), newTicketNumber(), newTicketNumber()};
-            t.addLine(line);
-        }
-        this.tickets.add(t);
+        Ticket withLines = this.addLines(t, numberOfLines);
+        this.tickets.add(withLines);
 
-        return ok(Json.toJson(t).toString());
+        return ok(Json.toJson(withLines).toString());
     }
 
     /**
      * Searches the collection of tickets using the ticket ID as the key
+     *
+     * Example: curl -X GET http://localhost:9000/ticket/3d8df83f-3b08-479b-b4ac-2aa542de0b58
      *
      * @param id the ticket to search for
      * @return if a corresponding ticket is found a http 200 response with a body containing the ticket,
@@ -59,6 +62,27 @@ public class LotteryController extends Controller {
         Ticket t = this.searchTicketsById(id);
         if (t != null) {
             return ok(Json.toJson(t).toString());
+        } else {
+            return notFound("Could not find specified ticket");
+        }
+    }
+
+    /**
+     * Adds the specified number of lines to the lottery ticket
+     *
+     * Example (adding 3 lines): curl -X PUT http://localhost:9000/ticket/3d8df83f-3b08-479b-b4ac-2aa542de0b58/3
+     *
+     * @param id the lottery ticket to modify
+     * @param numberOfLines the number of lines to add to the ticket
+     * @return if a corresponding ticket is found a http 200 response with a body containing the modified ticket,
+     * otherwise a http 400 (not found) is returned
+     */
+    public Result addLines(String id, int numberOfLines) {
+        Ticket t = this.searchTicketsById(id);
+        if (t != null) {
+            Ticket withLines = this.addLines(t, numberOfLines);
+            this.updateTicket(withLines);
+            return ok(Json.toJson(withLines).toString());
         } else {
             return notFound("Could not find specified ticket");
         }
@@ -85,5 +109,33 @@ public class LotteryController extends Controller {
                 .filter(ticket -> ticket.getId().equals(id))
                 .findAny()
                 .orElse(null);
+    }
+
+    /**
+     *  Adds a specified number of lines to the ticket
+     *
+     * @param ticket the lottery ticket to modify
+     * @param numberOfLines the number of lines to add to the ticket
+     * @return the modified ticket
+     */
+    private Ticket addLines(Ticket ticket, int numberOfLines) {
+        for (int i = 0; i < numberOfLines; i++) {
+            int[] line = {newTicketNumber(), newTicketNumber(), newTicketNumber()};
+            ticket.addLine(line);
+        }
+
+        return ticket;
+    }
+
+    /**
+     * Update the collection of tickets to reflect the change in a ticket
+     *
+     * @param ticket the updated ticket
+     */
+    private void updateTicket(Ticket ticket) {
+        Ticket toRemove = this.searchTicketsById(ticket.getId());
+
+        this.tickets.remove(toRemove);
+        this.tickets.add(ticket);
     }
 }
